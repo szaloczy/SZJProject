31e3fed27735d37d82a12b70929b50ac9e158764
SZKrisztian <szaloczy6@gmail.com>
2024-07-02 13:05:37 +0200

    completed-modify-function

--- backend/src/main/java/com/szj/demo/dtos/ProductUpdateDTO.java
+++ backend/src/main/java/com/szj/demo/dtos/ProductUpdateDTO.java
@@ -1,13 +1,34 @@
 package com.szj.demo.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.szj.demo.model.Product;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class ProductUpdateDTO {
 
+    @JsonProperty
     private Long productId;
+    @JsonProperty
     private String productName;
+    @JsonProperty
     private String productDescription;
+    @JsonProperty
     private Double price;
 
+    public ProductUpdateDTO(){};
+
+    public ProductUpdateDTO(Long productId, String name, String description, Double price) {
+        this.productId = productId;
+
+        if(name.isEmpty()) throw new IllegalArgumentException("Name cannot be empty!");
+        this.productName = name;
+
+        if (description.isEmpty()) throw new IllegalArgumentException("Description cannot be empty");
+        this.productDescription = description;
+
+        if (price < 0 ) throw new IllegalArgumentException("Price must be grater than zero!");
+        this.price = price;
+    }
 }
